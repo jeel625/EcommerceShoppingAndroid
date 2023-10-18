810b64a8a938c50fe0f6201ce55642191e7f107a
Adit Vaidya <aditvaidya@Adits-MacBook-Pro.local>
2023-10-17 22:03:20 -0400

    adminfilesadded

---
+++ app/src/main/java/com/example/shoppingapplicatonecommerce/AdminUsersProductsActivity.java
@@ -0,0 +1,14 @@
+package com.example.shoppingapplicatonecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminUsersProductsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_users_products);
+    }
+}
