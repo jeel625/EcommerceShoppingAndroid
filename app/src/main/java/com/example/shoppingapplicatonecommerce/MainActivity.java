9672c566e125fd0ce11d916217384f34ec93a226
Adit Vaidya <aditvaidya@Adits-MacBook-Pro.local>
2023-10-25 19:43:33 -0400

    Bug Fixed in Main Activity

--- app/src/main/java/com/example/shoppingapplicatonecommerce/MainActivity.java
+++ app/src/main/java/com/example/shoppingapplicatonecommerce/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                Intent intent = new Intent(MainActivity.this, AdminCategoryActivity.class);
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
                 startActivity(intent);
             }
         });
